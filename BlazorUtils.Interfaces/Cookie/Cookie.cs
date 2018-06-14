ec10b141af2b9ca87c8add2b2bfdbf6efd551b5b
15110123 <leminhtanvatc@yahoo.com.vn>
2018-06-15 03:26:56 +0700

    [Interface]: Minor change to class Cookie

--- BlazorUtils.Interfaces/Cookie/Cookie.cs
+++ BlazorUtils.Interfaces/Cookie/Cookie.cs
@@ -2,13 +2,34 @@
 
 namespace BlazorUtils.Interfaces.Cookie
 {
+    /// <summary>
+    /// Object representing cookie
+    /// </summary>
     public sealed class Cookie
     {
+        /// <summary>
+        /// Cookie key
+        /// </summary>
         public string Key { get; }
+        /// <summary>
+        /// Cookie value
+        /// </summary>
         public string Value { get; }
+        /// <summary>
+        /// Cookie expire days
+        /// </summary>
         public int Expires { get; }
+        /// <summary>
+        /// Cookie path
+        /// </summary>
         public string Path { get; }
-
+        /// <summary>
+        /// Initializing a cookie
+        /// </summary>
+        /// <param name="key">Cookie key</param>
+        /// <param name="value">Cookie value</param>
+        /// <param name="expires">Cookie expire days</param>
+        /// <param name="path">Cookie path</param>
         public Cookie(string key, string value, int expires = 0, string path = "/")
         {
             Key = key ?? throw new ArgumentNullException(nameof(key));
