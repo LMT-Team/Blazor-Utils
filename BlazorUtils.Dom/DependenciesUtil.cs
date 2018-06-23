c11910952053e092cbb6c12f9caf227b52316d12
15110123 <leminhtanvatc@yahoo.com.vn>
2018-06-23 21:41:08 +0700

    Finish LMTLightbox

---
+++ BlazorUtils.Dom/DependenciesUtil.cs
@@ -0,0 +1,13 @@
+﻿using static BlazorUtils.Dom.DomUtil;
+
+namespace BlazorUtils.Dom
+{
+    internal static class DependenciesUtil
+    {
+        internal static void GetFeatherlight()
+        {
+            //Get Css and Js over CDN
+            Eval("if(window.LMTCDNFeatherlightDone == undefined){let lmtCssNode3 = document.createElement(\"link\");lmtCssNode3.href = \"https://cdn.jsdelivr.net/gh/noelboss/featherlight/release/featherlight.min.css\";lmtCssNode3.rel = \"stylesheet\";document.head.appendChild(lmtCssNode3);$.getScript(\"https://cdn.jsdelivr.net/gh/noelboss/featherlight/release/featherlight.min.js\").done(() => {LMTCDNFeatherlightDone = true;});}");
+        }
+    }
+}
