f338ad4e720e100b74d8ffe15d7fae4a0d54fe5a
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-09-11 13:40:00 +0700

    Fix "'DependenciesUtil' is inaccessible due to its protection level" bug.

--- BlazorUtils.Dom/DependenciesUtil.cs
+++ BlazorUtils.Dom/DependenciesUtil.cs
@@ -2,9 +2,15 @@
 
 namespace BlazorUtils.Dom
 {
-    internal static class DependenciesUtil
+    /// <summary>
+    /// Use internally. Should not be used in your project.
+    /// </summary>
+    public static class DependenciesUtil
     {
-        internal static void GetFeatherlight()
+        /// <summary>
+        /// Use internally. Should not be used in your project.
+        /// </summary>
+        public static void GetFeatherlight()
         {
             //Get Css and Js over CDN
             Eval("if(window.LMTCDNFeatherlightDone == undefined){let lmtCssNode3 = document.createElement(\"link\");lmtCssNode3.href = \"https://cdn.jsdelivr.net/gh/noelboss/featherlight/release/featherlight.min.css\";lmtCssNode3.rel = \"stylesheet\";document.head.appendChild(lmtCssNode3);$.getScript(\"https://cdn.jsdelivr.net/gh/noelboss/featherlight/release/featherlight.min.js\").done(() => {LMTCDNFeatherlightDone = true;});}");
