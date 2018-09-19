42e97fc4c6b7a25000463ced9d8fbc3b83a5e5b6
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-09-19 21:49:06 +0700

    Add LMTCookieBoot.js and SessionStorage class

---
+++ BlazorUtils.Cookie/LMTCookieBoot.js
@@ -0,0 +1,5 @@
+﻿window.LMTCookie = {};
+window.LMTCookie.SessionStorage = {};
+window.LMTCookie.SessionStorage.Get = (key) => {
+    return eval(`window.sessionStorage.${key}`);
+};
---
+++ BlazorUtils.Cookie/SessionStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorUtils.Cookie
+{
+    public static class SessionStorage
+    {
+
+    }
+}
