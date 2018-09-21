2d8d05859463b1198667581a92c6f7d08e3720bb
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-09-21 15:15:57 +0700

    Update LMTCookieBoot.js

--- BlazorUtils.Cookie/LMTCookieBoot.js
+++ BlazorUtils.Cookie/LMTCookieBoot.js
@@ -1,5 +1,5 @@
-﻿window.LMTCookie = {};
-window.LMTCookie.SessionStorage = {};
-window.LMTCookie.SessionStorage.Get = (key) => {
+﻿window.LMTCookieBoot2 = {};
+window.LMTCookieBoot2.SessionStorage = {};
+window.LMTCookieBoot2.SessionStorage.Get = (key) => {
     return eval(`window.sessionStorage.${key}`);
 };
