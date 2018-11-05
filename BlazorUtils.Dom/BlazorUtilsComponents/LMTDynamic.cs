a072921547a88035e4b1f609568a3e2741c017de
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-11-05 12:29:32 +0700

    Dom: Add child content injecting ability to LMTDynamic

--- BlazorUtils.Dom/BlazorUtilsComponents/LMTDynamic.cs
+++ BlazorUtils.Dom/BlazorUtilsComponents/LMTDynamic.cs
@@ -2,8 +2,6 @@
 using Microsoft.AspNetCore.Blazor;
 using Microsoft.AspNetCore.Blazor.Components;
 using Microsoft.AspNetCore.Blazor.RenderTree;
-//using System;
-//using System.Reflection;
 
 namespace BlazorUtils.Dom.BlazorUtilsComponents
 {
@@ -17,54 +15,23 @@ namespace BlazorUtils.Dom.BlazorUtilsComponents
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
+            var seq = 0;
+
             //Open component
-            builder.OpenComponent<TComponent>(0);
+            builder.OpenComponent<TComponent>(seq++);
 
             //Set attribute
-            foreach (var (PropertyName, Value) in parameters)
+            for (var i = 0; i < parameters.Length; i++)
             {
-                //if (!property.CanWrite) continue;
+                builder.AddAttribute(seq++, parameters[i].PropertyName, parameters[i].Value);
+            }
 
-                builder.AddAttribute(0, PropertyName, Value);
+            if (ChildContent != null)
+            {
+                builder.AddAttribute(seq, "ChildContent", ChildContent);
             }
 
             builder.CloseComponent();
-
-            ////try
-            ////{
-            //Console.WriteLine("Get child property");
-
-            //    Console.WriteLine($"Number of frames: {builder.GetFrames().Array.Length}");
-
-            //    foreach (var frame in builder.GetFrames().Array)
-            //    {
-            //        if (frame.ElementName == null) continue;
-            //        Console.WriteLine("Frame component type: ");
-            //        Console.WriteLine(frame.ElementName);
-            //    }
-            //    //var childProperty = typeof(TComponent).GetProperty("ChildContent", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-
-            //    foreach (var property in builder.GetFrames().Array[0].Component.GetType().GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance))
-            //    {
-            //        Console.WriteLine(property.Name);
-            //    }
-
-            //    foreach (var property in typeof(TComponent).GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance))
-            //    {
-            //        if (property.Name != "ChildContent") continue;
-
-            //        property.SetValue(builder.GetFrames().Array[0].Component, ChildContent);
-            //    }
-
-            //    Console.WriteLine(builder.GetFrames().Array[0].Component.GetType());
-
-            //    Console.WriteLine("End dynamic");
-            ////}
-            ////No ChildContent property
-            ////catch (Exception e)
-            ////{
-            ////    throw e;
-            ////}
         }
     }
 }
