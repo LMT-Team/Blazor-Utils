987950ffef25954052ab4d2586168814864882f4
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2019-03-03 23:31:26 +0700

    Add IsolateTesting

--- BlazorUtils.WebTest.0.6.App/Startup.cs
+++ BlazorUtils.WebTest.0.6.App/Startup.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Blazor.Services;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 
 namespace BlazorUtils.WebTest._0._6.App
 {
@@ -9,6 +12,16 @@ namespace BlazorUtils.WebTest._0._6.App
         {
             // Since Blazor is running on the server, we can use an application service
             // to read the forecast data.
+
+            services.AddScoped<HttpClient>(s =>
+            {
+                // Creating the URI helper needs to wait until the JS Runtime is initialized, so defer it.
+                var uriHelper = s.GetRequiredService<IUriHelper>();
+                return new HttpClient
+                {
+                    BaseAddress = new Uri(uriHelper.GetBaseUri())
+                };
+            });
         }
 
         public void Configure(IBlazorApplicationBuilder app)
