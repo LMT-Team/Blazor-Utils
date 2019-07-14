f1971588afe9b6b3faedfeda77e041a1e4e40956
tan.le <leminhtanvatc@outlook.com>
2019-07-14 14:14:36 +0700

    LMTInjector to use System.Text.Json.

---
+++ Demo.ClientSide/Shared/Components/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BlazorUtils.Interfaces.BlazorComponents;
+
+namespace Demo.ClientSide.Shared.Components
+{
+    public class Player : LMTDynamicContext
+    {
+        public string QueryStringParam { get; set; }
+
+        public Player(string queryStringParam)
+        {
+            QueryStringParam = queryStringParam;
+        }
+
+        public override Type ComponentSwitch()
+        {
+            if (QueryStringParam == null)
+                return typeof(AdultPlayer);
+            return typeof(KidPlayer);
+        }
+
+        public override (string PropertyName, object Value)[] ParametersSwitch()
+        {
+            return new (string, object)[] {
+                  ("url", "https://tempuri.org/videos/id=123"),
+                  ("starttime", 0),
+                  ("repeat", false)};
+        }
+    }
+}
