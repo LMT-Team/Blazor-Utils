3630fc62e0319b06a00343cfdfcb3bf8efa8de2d
tan.le <leminhtanvatc@outlook.com>
2019-07-15 23:33:57 +0700

    Init Cli project

---
+++ Tools/BlazorUtils.Cli/Program.cs
@@ -0,0 +1,25 @@
+﻿using BlazorUtils.Cli.Options;
+using CommandLine;
+using System;
+
+namespace BlazorUtils.Cli
+{
+    class Program
+    {
+        static Parser _Parser = Parser.Default;
+
+        static void Main(string[] args)
+        {
+            _Parser
+           .ParseArguments<CreateOptions>(args)
+           .WithParsed<CreateOptions>(o =>
+           {
+               Console.WriteLine(o.ProjectName);
+               Console.WriteLine(o.Target);
+
+               foreach (var package in o.Packages)
+                   Console.WriteLine(package);
+           });
+        }
+    }
+}
